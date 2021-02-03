0867102c56bd94659fdaa7c27d1a118df16229c3
stringmanolo <manuelvarelacaldas@gmail.com>
2021-02-03 06:11:38 +0100

    global scriptArgs and more tests in test.js

--- test.js
+++ test.js
@@ -8,7 +8,15 @@ console.log(std.evalScript("8*8"));
 let text = std.loadFile("./helloWorld.js");
 console.log(text);
 
-loadScript("./helloWorld.js");
+//loadScript("./helloWorld.js");
+
+let fd = std.open("./dummy.txt", "w");
+fd.puts("abc");
+fd.close();
+
+for(let i in scriptArgs) {
+  console.log(scriptArgs[i]);
+}
 
 std.exit(1);
 
